92f3f6b776d011e77fde306331153de345ef280f
tiantiankl <god1166@gmail.com>
2025-08-28 13:57:12 +0800

    vault backup: 2025-08-28 13:57:12

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Blog demo",
+    pageTitle: "我的日常",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
